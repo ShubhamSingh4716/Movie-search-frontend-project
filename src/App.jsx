12dd257c48fda0e3da09647d70a4390f19e622d1
Shubham Singh <singhshubham4716@gmail.com>
2023-05-08 16:30:32 +0530

    Movie search frontend functionality

---
+++ src/App.jsx
@@ -0,0 +1,81 @@
+import Result from "./components/Nav";
+import axios from "axios";
+import { useState } from "react";
+import { useEffect } from "react";
+import "./App.css"
+const APIURL = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
+const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
+function App() {
+  const [movies, setMovies] = useState([]);
+  const [search, setSearch] = useState("");
+
+  const changeTheSearch = (event) => {
+    // console.log(event.target.value);
+    setSearch(event.target.value);
+  }
+
+  const getAllMovies = () => {
+    axios.get(APIURL)
+      .then(
+        (response) => {
+          console.log(response.data.results)
+          setMovies(response.data.results);
+        }
+      )
+      .catch(
+        (error) => {
+          console.log(error)
+        }
+      )
+  }
+
+  const getSearchedMovies = () => {
+    // console.log(SEARCHAPI + search)
+    axios.get(
+      SEARCHAPI + search
+    )
+      .then(
+        (response) => {
+          console.log(response.data.results)
+          setMovies(response.data.results);
+        }
+      )
+      .catch(
+        (error) => { 
+          console.log(error);
+        }
+      )
+  }
+
+  useEffect(
+    () => {
+      setMovies([]);
+      // console.log("Hello");
+      if (search === "") {
+        getAllMovies();
+      } else {
+        getSearchedMovies();
+      }
+    },
+    [search]
+  )
+
+  return (
+    <div className="max-w-[1240px] shadow-xl min-h-[400px] mx-auto p-3" >
+      <div id="heading"><header className="Header"><h1>THEMOVIEUNLEASH.com</h1></header></div>
+
+      <input type="search" placeholder=" Enter Movies Name" value={search} onChange={changeTheSearch} className="w-full border border-black rounded text-slate-700 p-4" id="SearchBox"></input>
+      {
+        movies.length === 0
+          ?
+          <div className="text-3xl text-center mt-2"> Loading... </div>
+          :
+          <Result movies={movies} />
+
+      }
+      
+    </div>
+  );
+}
+
+export default App;
